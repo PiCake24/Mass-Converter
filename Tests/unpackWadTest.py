4917ad0f1670fbf10139ced703f1ede17370e0a1
Yannik Schmiede <yannikschmiede@gmail.com>
2024-07-14 12:44:36 +0200

    I actually have no idea

--- Tests/unpackWadTest.py
+++ Tests/unpackWadTest.py
@@ -9,12 +9,10 @@ from MainControl.unpackChampions import unpack_all_wad
 class TestFileHandler(unittest.TestCase):
     def setUp(self):
         self.folder_path = r'D:\Riot Games\League of Legends\Game\DATA\FINAL\Champions\akali.wad'
-
-    def test_method_x(self):
-        # Check if the file exists and delete it if it does
         if os.path.exists(self.folder_path):
             shutil.rmtree(self.folder_path)
 
+    def test_method_x(self):
         # Call method x which should create the file
         champion_map = {"akali": 1}
         python_path = r"C:\Users\Yanni\AppData\Local\Programs\Python\Python310\Scripts"
