936186231c338269b40fee6d063291821e588869
Yannik Schmiede <yannikschmiede@gmail.com>
2024-08-02 11:59:10 +0200

    Implementing writing into files

---
+++ MainControl/WriteIntoFile.py
@@ -0,0 +1,24 @@
+#copy file and write it into it again
+# change size
+def write_into(scale):
+    lines = []
+    with open(r"D:\Riot Games\League of Legends\Game\DATA\FINAL\Champions\data\characters\akali\skins\skin0.py", "r") as file:
+        for line in file:
+            lines.append(line)
+            if line.strip() == "skinMeshProperties: embed = SkinMeshDataProperties {":
+                break
+        for _ in range(3):
+            line = file.readline()
+            lines.append(line)
+        lines.append("\t \t \tskinScale: f32 = " + str(scale)+ "\n")
+        for line in file:
+            if line.__contains__("skinScale"):
+                continue
+            lines.append(line)
+
+    output = "".join(lines)
+    print(output)
+    with open(r"D:\Riot Games\League of Legends\Game\DATA\FINAL\Champions\data\characters\akali\skins\skin0.py", "w") as file:
+        file.write(output)
+
+write_into(3)
